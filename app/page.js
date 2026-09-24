08f47bdc2283e5ea23a50a81c33189d4cabf85f6
issaamah2390-rgb <issaamah2390@gmail.com>
2026-09-24 21:38:08 +0100

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, textAlign: "center" }}>
-      <h1>مرحباً بكم في المتجر</h1>
-      <p>الموقع شغال تمام ✅</p>
-    </main>
-  );
-}
